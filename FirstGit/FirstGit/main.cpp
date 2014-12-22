9250c7d0b5cf2d59ea584b78f16c9292d7752dd3
SylviaShen <sylviashen@SylviaShendeMacBook-Air.local>
2014-12-22 21:15:50 +0800

    first Try

---
+++ FirstGit/FirstGit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstGit
+//
+//  Created by SylviaShen on 14-12-22.
+//  Copyright (c) 2014年 SylviaShen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
